515536c95291a8c074eee346c7bf3363bc45c906
legendholding192025 <mufeed.rahman@legendholding.com>
2025-06-26 12:31:26 +0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "legendx",
   description: "We're working hard to bring you something amazing. Stay tuned for the launch!",
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
