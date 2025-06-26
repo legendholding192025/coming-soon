9b573ba7886eab7ee99a51ca3e688ef93b4ace9d
legendholding192025 <mufeed.rahman@legendholding.com>
2025-06-26 12:26:49 +0400

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Coming Soon",
+  title: "legendx",
   description: "We're working hard to bring you something amazing. Stay tuned for the launch!",
 };
 
